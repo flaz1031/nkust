424b4a6e294da23d63734f028d7fc455db8f3c55
flaz1031 <93572154+flaz1031@users.noreply.github.com>
2021-11-16 14:07:12 +0800

    Add files via upload

---
+++ bmi.java
@@ -0,0 +1,9 @@
+class bmi {
+    public static void main(String[] args) {
+        float h = 1.73f;
+        int w = 81;
+        float bmi = w / (h * h);
+
+        System.out.println(bmi);
+    }
+}
